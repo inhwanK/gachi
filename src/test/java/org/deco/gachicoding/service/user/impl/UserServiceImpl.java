c53989a7381e42c85bc94b2f4c70c240c815bb69
inhwanK <inhan1009@naver.com>
2022-04-09 06:53:52 +0900

    Fix(UserServiceImpl) : PasswordEncoderFactories delegatingencoder 생성 주석 추가

--- src/test/java/org/deco/gachicoding/service/user/impl/UserServiceImpl.java
+++ src/test/java/org/deco/gachicoding/service/user/impl/UserServiceImpl.java
@@ -16,6 +16,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -29,6 +30,7 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final AuthService authService;
     private final PasswordEncoder passwordEncoder;
+//            = PasswordEncoderFactories.createDelegatingPasswordEncoder();
     private final AuthenticationManager authenticationManager;
     private final JwtTokenProvider jwtTokenProvider;
 

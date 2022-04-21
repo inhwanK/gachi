d7a5f67a067186f04a70f56966acd2375b4d95d8
inhwanK <inhan1009@naver.com>
2022-04-22 02:16:24 +0900

    Feat(Board) : Board 엔터티, 리포지토리 추가

---
+++ src/main/java/org/deco/gachicoding/domain/board/BoardRepository.java
@@ -0,0 +1,9 @@
+package org.deco.gachicoding.domain.board;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    Page<Board> findAllByOrderByBoardIdxAsc();
+}

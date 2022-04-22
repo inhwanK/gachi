f534402a46cf71be5581bb4e9ddb35b50757fe16
inhwanK <inhan1009@naver.com>
2022-04-22 16:30:47 +0900

    Feat(Board api) : dto, service, controller 추가

--- src/main/java/org/deco/gachicoding/domain/board/BoardRepository.java
+++ src/main/java/org/deco/gachicoding/domain/board/BoardRepository.java
@@ -1,9 +1,10 @@
 package org.deco.gachicoding.domain.board;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
-    Page<Board> findAllByOrderByBoardIdxAsc();
+    Page<Board> findAllByOrderByBoardIdxAsc(Pageable pageable);
 }

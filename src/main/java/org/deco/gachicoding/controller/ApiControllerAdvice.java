1fdc146a305257167189ab4f563db5c2ce6dcb87
YeongJun Seo <ay9564@naver.com>
2022-05-11 15:24:53 +0900

    Feat(ResponseState) : ResponseEntity를 위한 ResponseState class 추가 Feat(ExceptionHandler) : ConstraintViolationException, DataIntegrityViolationException, CustomException 추가 Fix(BoardController) : ResponseType void -> ResponseEntity 타입 변경

--- src/main/java/org/deco/gachicoding/controller/ApiControllerAdvice.java
+++ src/main/java/org/deco/gachicoding/controller/ApiControllerAdvice.java
@@ -1,17 +1,24 @@
 package org.deco.gachicoding.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.deco.gachicoding.dto.response.CustomException;
+import org.deco.gachicoding.dto.response.ResponseState;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.validation.ConstraintViolationException;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.deco.gachicoding.dto.response.StatusEnum.DUPLICATE_RESOURCE;
+
+@Slf4j
 @RestControllerAdvice
 public class ApiControllerAdvice {
-
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException e) {
         Map<String, String> errors = new HashMap<>();
@@ -19,5 +26,17 @@ public class ApiControllerAdvice {
                 .forEach(c -> errors.put(((FieldError) c).getField(), c.getDefaultMessage()));
 
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(value = { ConstraintViolationException.class, DataIntegrityViolationException.class})
+    protected ResponseEntity<ResponseState> handleDataException() {
+        log.error("handleDataException throw Exception : {}", DUPLICATE_RESOURCE);
+        return ResponseState.toResponseEntity(DUPLICATE_RESOURCE);
+    }
+
+    @ExceptionHandler(CustomException.class)
+    protected ResponseEntity<ResponseState> handleCustomException(CustomException e) {
+        log.error("handleCustomException throw CustomException : {}", e.getStatusEnum());
+        return ResponseState.toResponseEntity(e.getStatusEnum());
     }
 }
--- src/main/java/org/deco/gachicoding/service/board/impl/BoardServiceImpl.java
+++ src/main/java/org/deco/gachicoding/service/board/impl/BoardServiceImpl.java
@@ -3,14 +3,19 @@ package org.deco.gachicoding.service.board.impl;
 import lombok.RequiredArgsConstructor;
 import org.deco.gachicoding.domain.board.Board;
 import org.deco.gachicoding.domain.board.BoardRepository;
+import org.deco.gachicoding.dto.response.CustomException;
+import org.deco.gachicoding.dto.response.ResponseState;
 import org.deco.gachicoding.dto.board.BoardResponseDto;
 import org.deco.gachicoding.dto.board.BoardSaveRequestDto;
 import org.deco.gachicoding.dto.board.BoardUpdateRequestDto;
 import org.deco.gachicoding.service.board.BoardService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.deco.gachicoding.dto.response.StatusEnum.*;
 
 @Service
 @RequiredArgsConstructor
@@ -20,9 +25,10 @@ public class BoardServiceImpl implements BoardService {
 
     @Transactional
     @Override
-    public Long registerBoard(BoardSaveRequestDto dto) {
+    public ResponseEntity<ResponseState> registerBoard(BoardSaveRequestDto dto) {
+        boardRepository.save(dto.toEntity());
 
-        return boardRepository.save(dto.toEntity()).getBoardIdx();
+        return ResponseState.toResponseEntity(SAVE_SUCCESS);
     }
 
     @Transactional
@@ -37,9 +43,8 @@ public class BoardServiceImpl implements BoardService {
     @Transactional
     @Override
     public BoardResponseDto getBoardDetail(Long boardIdx) {
-
         Board entity = boardRepository.findById(boardIdx)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시글 입니다. boardIdx = " + boardIdx));
+                .orElseThrow(() -> new CustomException(RESOURCE_NOT_EXIST));
 
         return new BoardResponseDto(entity);
     }
@@ -49,7 +54,7 @@ public class BoardServiceImpl implements BoardService {
     public BoardResponseDto modifyBoard(BoardUpdateRequestDto dto) {
         Long boardIdx = dto.getBoardIdx();
         Board board = boardRepository.findById(boardIdx)
-                .orElseThrow(() -> new IllegalArgumentException("게시글이 존재하지 않습니다. 글번호 = " + boardIdx));
+                .orElseThrow(() -> new CustomException(RESOURCE_NOT_EXIST));
 
         board = board.update(dto.getBoardTitle(), dto.getBoardContent());
 
@@ -62,29 +67,34 @@ public class BoardServiceImpl implements BoardService {
 
     @Override
     @Transactional
-    public void disableBoard(Long boardIdx) {
+    public ResponseEntity<ResponseState> disableBoard(Long boardIdx) {
         Board board = boardRepository.findById(boardIdx)
-                .orElseThrow(() -> new IllegalArgumentException("게시글이 존재하지 않습니다. 글번호 = " + boardIdx));
+                .orElseThrow(() -> new CustomException(RESOURCE_NOT_EXIST));
 
         board.disableBoard();
+
+        return ResponseState.toResponseEntity(DISABLE_SUCCESS);
     }
 
     @Override
     @Transactional
-    public void enableBoard(Long boardIdx) {
+    public ResponseEntity<ResponseState> enableBoard(Long boardIdx) {
         Board board = boardRepository.findById(boardIdx)
-                .orElseThrow(() -> new IllegalArgumentException("게시글이 존재하지 않습니다. 글번호 = " + boardIdx));
+                .orElseThrow(() -> new CustomException(RESOURCE_NOT_EXIST));
 
         board.enableBoard();
+
+        return ResponseState.toResponseEntity(ENABLE_SUCCESS);
     }
 
     @Override
     @Transactional
-    public Long removeBoard(Long boardIdx) {
+    public ResponseEntity<ResponseState> removeBoard(Long boardIdx) {
         Board board = boardRepository.findById(boardIdx)
-                .orElseThrow(() -> new IllegalArgumentException("게시글이 존재하지 않습니다. 글번호 = " + boardIdx));
+                .orElseThrow(() -> new CustomException(RESOURCE_NOT_EXIST));
 
         boardRepository.delete(board);
-        return boardIdx;
+
+        return ResponseState.toResponseEntity(REMOVE_SUCCESS);
     }
 }

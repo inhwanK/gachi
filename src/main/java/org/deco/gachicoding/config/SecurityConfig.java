4bb2e94f0ebf531cd3c8bf1fdf8f7eeb313de3f2
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-05-13 04:01:20 +0900

    Chore(SecurityConfig) : 인코더 빈 생성 방식에 대한 주석 추가

--- src/main/java/org/deco/gachicoding/config/SecurityConfig.java
+++ src/main/java/org/deco/gachicoding/config/SecurityConfig.java
@@ -7,13 +7,20 @@ import org.deco.gachicoding.domain.user.UserRole;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 // api에도 시큐리티 접근 권한 설정이 먹힘
@@ -25,7 +32,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final JwtTokenProvider jwtTokenProvider;
 
-    // 회원가입 시 비밀번호 암호화에 사용할 Encode 빈 등록
+
+    /*
+     * 레퍼런스에서 권장하는 인코더 생성 방법
+     * PasswordEncoderFactories.createDelegatingPasswordEncoder()
+     * {bcrypt}1234 방식으로 비밀번호를 저장해야 함. 인코딩방식이 변화되어도 계속해서 사용할 수 있음.
+     */
     @Bean
     public BCryptPasswordEncoder encoderPassword() {
         return new BCryptPasswordEncoder();
@@ -40,8 +52,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests().antMatchers()
                 .authenticated().anyRequest()
                 .permitAll();
-
-
     }
 
     @Bean

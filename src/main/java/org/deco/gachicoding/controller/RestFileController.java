1ebfe1abb37ffdca85f98a900c2fbad073a81b6b
YeongJun Seo <ay9564@naver.com>
2022-05-20 20:03:20 +0900

    Fix(board) : boolean -> Boolean 변수 타입 변경

--- src/main/java/org/deco/gachicoding/controller/RestFileController.java
+++ src/main/java/org/deco/gachicoding/controller/RestFileController.java
@@ -2,7 +2,6 @@ package org.deco.gachicoding.controller;
 
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.deco.gachicoding.service.impl.FileServiceImpl;
 import org.deco.gachicoding.service.impl.S3ServiceImpl;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,7 +9,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
-// 리팩토링 필요 구현 덜 됬다 건들면 손모가지,,
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor

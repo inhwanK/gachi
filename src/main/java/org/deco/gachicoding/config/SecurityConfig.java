65598eb14ee712063d9f5aaafa4fc987a82927ce
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-05-24 02:35:37 +0900

    Chore(SecurityConfig) : 주석 추가

--- src/main/java/org/deco/gachicoding/config/SecurityConfig.java
+++ src/main/java/org/deco/gachicoding/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import java.util.Arrays;
 import java.util.Collections;
 
 // 시큐리티 설정 관련 자료 : https://velog.io/@seongwon97/Spring-Security-Filter%EB%9E%80
+// 백기선 시큐리티 강의 : https://youtu.be/fG21HKnYt6g
 @Configuration
 @EnableWebSecurity(debug = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {

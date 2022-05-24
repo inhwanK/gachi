641072cf7150dd537563324906dc3a65974adc48
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-05-25 03:05:42 +0900

    Fix(UserDetails) : UserDetails, UserDetailsService migration

--- src/main/java/org/deco/gachicoding/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/org/deco/gachicoding/service/impl/UserDetailsServiceImpl.java
@@ -3,12 +3,15 @@ package org.deco.gachicoding.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.deco.gachicoding.domain.user.User;
 import org.deco.gachicoding.domain.user.UserRepository;
+import org.deco.gachicoding.domain.user.UserRole;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 @RequiredArgsConstructor
@@ -22,6 +25,47 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         User user = userRepository.findByUserEmail(email)
                 .orElseThrow(()-> new UsernameNotFoundException("등록되지 않은 사용자 입니다"));
 
-        return new UserDetailsImpl(user);
+        UserDetails userDetails = new UserDetails() {
+            @Override
+            public Collection<? extends GrantedAuthority> getAuthorities() {
+                UserRole role = user.getUserRole();
+                SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + role.toString());
+                Collection<GrantedAuthority> authorities = new ArrayList<>(); // List인 이유 : 여러개의 권한을 가질 수 있다
+                authorities.add(authority);
+                return authorities;
+            }
+
+            @Override
+            public String getPassword() {
+                return user.getUserPassword();
+            }
+
+            @Override
+            public String getUsername() {
+                return user.getUserName();
+            }
+
+            @Override
+            public boolean isAccountNonExpired() {
+                return true;
+            }
+
+            @Override
+            public boolean isAccountNonLocked() {
+                return true;
+            }
+
+            @Override
+            public boolean isCredentialsNonExpired() {
+                return true;
+            }
+
+            @Override
+            public boolean isEnabled() {
+                return true;
+            }
+        };
+
+        return userDetails;
     }
 }
--- src/main/java/org/deco/gachicoding/service/impl/UserServiceImpl.java
+++ src/main/java/org/deco/gachicoding/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.deco.gachicoding.domain.auth.Auth;
 import org.deco.gachicoding.domain.user.User;
 import org.deco.gachicoding.domain.user.UserRepository;
+import org.deco.gachicoding.domain.user.UserRole;
 import org.deco.gachicoding.dto.user.LoginRequestDto;
 import org.deco.gachicoding.dto.user.UserResponseDto;
 import org.deco.gachicoding.dto.user.UserSaveRequestDto;
@@ -14,16 +15,23 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
 
     private final UserRepository userRepository;
     private final AuthService authService;
@@ -129,5 +137,54 @@ public class UserServiceImpl implements UserService {
     public Long deleteUser(Long idx) {
         userRepository.deleteById(idx);
         return idx;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByUserEmail(email)
+                .orElseThrow(()-> new UsernameNotFoundException("등록되지 않은 사용자 입니다"));
+
+        UserDetails userDetails = new UserDetails() {
+            @Override
+            public Collection<? extends GrantedAuthority> getAuthorities() {
+                UserRole role = user.getUserRole();
+                SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + role.toString());
+                Collection<GrantedAuthority> authorities = new ArrayList<>(); // List인 이유 : 여러개의 권한을 가질 수 있다
+                authorities.add(authority);
+                return authorities;
+            }
+
+            @Override
+            public String getPassword() {
+                return user.getUserPassword();
+            }
+
+            @Override
+            public String getUsername() {
+                return user.getUserName();
+            }
+
+            @Override
+            public boolean isAccountNonExpired() {
+                return true;
+            }
+
+            @Override
+            public boolean isAccountNonLocked() {
+                return true;
+            }
+
+            @Override
+            public boolean isCredentialsNonExpired() {
+                return true;
+            }
+
+            @Override
+            public boolean isEnabled() {
+                return true;
+            }
+        };
+
+        return userDetails;
     }
 }

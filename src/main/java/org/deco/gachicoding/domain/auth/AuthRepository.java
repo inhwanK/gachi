7e081aa7aa610996b8b617ac5c9a8cf43ca8de00
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-06-05 20:15:41 +0900

    Update(AuthRepository) : UUID를 통한 Auth 조회 추가

--- src/main/java/org/deco/gachicoding/domain/auth/AuthRepository.java
+++ src/main/java/org/deco/gachicoding/domain/auth/AuthRepository.java
@@ -1,6 +1,5 @@
 package org.deco.gachicoding.domain.auth;
 
-import org.apache.tomcat.jni.Local;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
@@ -9,7 +8,6 @@ import java.util.UUID;
 
 public interface AuthRepository extends JpaRepository<Auth, String> {
 
-//    Optional<UUID> findByTokenAndExpirationDateAfterAndExpired(String confirmationTokenId, LocalDateTime now, boolean expired);
     Optional<Auth> findByAuthEmailAndAuthExpdateAfterAndExpiredIsFalse(String email, LocalDateTime now);
-    Optional<Auth> findByAuthEmail(String email);
+    Optional<Auth> findByAuthToken(UUID authToken);
 }

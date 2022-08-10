98d4d97401c3972bb05addf10f60518cfca42432
YeongJun Seo <ay9564@naver.com>
2022-08-10 18:42:34 +0900

    #56 Refactor(Notice) : 공지사항 목록 리팩토링 Related to : #56

--- src/main/java/org/deco/gachicoding/post/notice/dto/NoticeDtoAssembler.java
+++ src/main/java/org/deco/gachicoding/post/notice/dto/NoticeDtoAssembler.java
@@ -2,7 +2,12 @@ package org.deco.gachicoding.post.notice.dto;
 
 import org.deco.gachicoding.post.notice.domain.Notice;
 import org.deco.gachicoding.post.notice.dto.request.NoticeSaveRequestDto;
+import org.deco.gachicoding.post.notice.dto.response.NoticeResponseDto;
 import org.deco.gachicoding.user.domain.User;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 public class NoticeDtoAssembler {
 
@@ -14,6 +19,24 @@ public class NoticeDtoAssembler {
                 .notTitle(dto.getNotTitle())
                 .notContents(dto.getNotContents())
                 .notPin(dto.getNotPin())
+                .build();
+    }
+
+    public static List<NoticeResponseDto> noticeResponseDtos(List<Notice> notices) {
+        return notices.stream()
+                .map(notice -> convertForm(notice))
+                .collect(toList());
+    }
+
+    private static NoticeResponseDto convertForm(Notice notice) {
+        return NoticeResponseDto.builder()
+                .notIdx(notice.getNotIdx())
+                .userEmail(notice.getWriterEmail())
+                .userNick(notice.getWriterNick())
+                .notTitle(notice.getNotTitle())
+                .notContents(notice.getNotContents())
+                .notViews(notice.getNotViews())
+                .notRegdate(notice.getNotRegdate())
                 .build();
     }
 }
--- src/main/java/org/deco/gachicoding/post/notice/dto/response/NoticeResponseDto.java
+++ src/main/java/org/deco/gachicoding/post/notice/dto/response/NoticeResponseDto.java
@@ -1,51 +1,42 @@
-//package org.deco.gachicoding.post.notice.dto.response;
-//
-//import lombok.Builder;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//import org.deco.gachicoding.post.notice.domain.Notice;
-//import org.deco.gachicoding.user.domain.User;
-//import org.deco.gachicoding.tag.dto.TagResponse;
-//import org.deco.gachicoding.tag.dto.response.TagResponseDto;
-//
-//import java.time.LocalDateTime;
-//import java.util.List;
-//
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//public class NoticeResponseDto implements TagResponse {
-//    private Long notIdx;
-//
-//    private String userEmail;
-//    private String userNick;
-//
-//    private String notTitle;
-//    private String notContent;
-//    private Long notViews;
-//    private LocalDateTime notRegdate;
-//
+package org.deco.gachicoding.post.notice.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.deco.gachicoding.user.domain.User;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+public class NoticeResponseDto {
+    private Long notIdx;
+
+    private String userEmail;
+    private String userNick;
+
+    private String notTitle;
+    private String notContent;
+    private Long notViews;
+    private LocalDateTime notRegdate;
+
 //    private List<TagResponseDto> tags;
-//
-//    @Builder
-//    public NoticeResponseDto(Notice notice) {
-//        this.notIdx = notice.getNotIdx();
-//        setWriterInfo(notice.getWriter());
-//
-//        this.notTitle = notice.getNotTitle();
-//        this.notContent = notice.getNotContent();
-//        this.notViews = notice.getNotViews();
-//        this.notRegdate = notice.getNotRegdate();
-//    }
-//
-//    private void setWriterInfo(User user) {
-//        this.userEmail = user.getUserEmail();
-//        this.userNick = user.getUserNick();
-//    }
-//
+
+    @Builder
+    public NoticeResponseDto(Long notIdx, String userEmail, String userNick, String notTitle, String notContents, Long notViews, LocalDateTime notRegdate) {
+        this.notIdx = notIdx;
+
+        this.userEmail = userEmail;
+        this.userNick = userNick;
+
+        this.notTitle = notTitle;
+        this.notContent = notContents;
+        this.notViews = notViews;
+        this.notRegdate = notRegdate;
+    }
+
 //    @Override
 //    public void setTags(List<TagResponseDto> tags) {
 //        this.tags = tags;
 //    }
-//}
+}

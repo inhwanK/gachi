913e469bb0bc3fa33e12241acd6e6e716a61e024
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-08-12 05:56:32 +0900

    Refactor(UserService) : 인증 관련 로직 추가
    UserDetailsService의 loadByUsername 메서드 구현
    securityConfig 인증 객체 주입
    
    Related to : #41

--- src/main/java/org/deco/gachicoding/config/SecurityConfig.java
+++ src/main/java/org/deco/gachicoding/config/SecurityConfig.java
@@ -1,13 +1,14 @@
 package org.deco.gachicoding.config;
 
+import org.deco.gachicoding.user.application.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
@@ -19,29 +20,32 @@ import java.util.Arrays;
 // 시큐리티 설정 관련 자료 : https://velog.io/@seongwon97/Spring-Security-Filter%EB%9E%80
 // 백기선 시큐리티 강의 : https://youtu.be/fG21HKnYt6g
 @Configuration
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity(debug = false)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Value("${key.value}")
     private String frontHost;
 
+    @Autowired
+    private UserService userService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService);
+    }
+
     protected void configure(HttpSecurity http) throws Exception {
 
         http
                 .cors().configurationSource(corsConfigurationSource())
-            .and()
+                .and()
                 .csrf().disable()
                 .headers().frameOptions().disable()
-            .and()
+                .and()
                 .authorizeRequests().antMatchers()
                 .authenticated().anyRequest()
                 .permitAll();
 
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
     }
 
     @Bean
@@ -59,9 +63,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000","http://localhost:3001", frontHost));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3001", frontHost));
         configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Accept", "X-Requested-With", "remember-me", "accesss-token", "Set-Cookie"));
-        configuration.setAllowedMethods(Arrays.asList("DELETE","GET","HEAD","OPTIONS","PATCH","POST","PUT"));
+        configuration.setAllowedMethods(Arrays.asList("DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"));
         configuration.setAllowCredentials(true);
         configuration.setMaxAge(3600L);
 

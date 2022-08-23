3faf3df12199300dd0a6a193746a60f78970c247
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-08-24 01:21:17 +0900

    Chore(GachicodingApplicationTests) : EnableJpaAuditing 어노테이션 추가 (#41)

--- src/test/java/org/deco/gachicoding/GachicodingApplicationTests.java
+++ src/test/java/org/deco/gachicoding/GachicodingApplicationTests.java
@@ -2,7 +2,9 @@ package org.deco.gachicoding;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootTest
 class GachicodingApplicationTests {
 

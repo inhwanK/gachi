b52be4f1d4c748bde6bf87340755996ec9570764
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-08-26 04:44:40 +0900

    Test() : 유저 저장, 삭제 테스트 추가 (#41, #81)

--- src/test/java/org/deco/gachicoding/unit/user/domain/UserRepositoryTest.java
+++ src/test/java/org/deco/gachicoding/unit/user/domain/UserRepositoryTest.java
@@ -1,12 +1,17 @@
 package org.deco.gachicoding.unit.user.domain;
 
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.deco.gachicoding.user.domain.User;
 import org.deco.gachicoding.user.domain.repository.UserRepository;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -25,10 +30,20 @@ public class UserRepositoryTest {
     @BeforeEach
     void setUp() {
         user = MockUser.builder()
+                .userIdx(1L)
                 .userEmail("inhan1009@naver.com")
                 .build();
 
         userRepository.save(user);
+    }
+
+    @AfterEach
+    void tearDown() {
+
+        userRepository.deleteAll();
+//        testEntityManager.getEntityManager()
+//                .createNativeQuery("ALTER TABLE user RESTART IDENTITY")
+//                .executeUpdate();
     }
 
     @DisplayName("유저 이메일로 유저를 조회한다.")
@@ -46,19 +61,42 @@ public class UserRepositoryTest {
         assertThat(userRepository.findByUserEmail("invalidUser")).isEmpty();
     }
 
-    @Disabled
     @DisplayName("유저를 저장한다.")
     @Test
-    public void userSaveTest() {
+    public void save_Success() {
+        User saveUser = MockUser.builder()
+                .userEmail("saveTestEmail@test.com")
+                .userNick("saveTestNick")
+                .build();
 
-        assertThat(false).isTrue();
+        Long saveIdx = userRepository.save(saveUser).getUserIdx();
+
+        Optional<User> retrievedUser = userRepository.findById(saveIdx);
+
+        assertThat(saveUser).isEqualTo(retrievedUser.get());
     }
 
-    @Disabled
-    @DisplayName("유저를 삭제한다.")
+    @DisplayName("중복된 이메일로 유저를 저장할 수 없다.")
     @Test
-    public void deleteUserTest() {
-        assertThat(false).isTrue();
+    public void save_DuplicateEmail_Failed() {
+        User duplicateEmailUser = MockUser.builder()
+                .userEmail("inhan1009@naver.com")
+                .userNick("saveTestNick")
+                .build();
+
+        assertThatThrownBy(
+                () -> userRepository.save(duplicateEmailUser)
+        ).isInstanceOf(DataIntegrityViolationException.class);
     }
 
+    @DisplayName("유저번호로 유저를 삭제한다.")
+    @Test
+    public void deleteById_Success() {
+        userRepository.deleteById(1L);
+
+        assertThatThrownBy(
+                () -> userRepository.findById(1L)
+                        .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."))
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
 }

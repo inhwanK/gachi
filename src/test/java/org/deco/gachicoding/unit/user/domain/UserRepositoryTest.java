f24c7834bc1531439f6750d837a92c12843a1167
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-08-27 05:07:01 +0900

    Test(User, MockUser) : UserRepository 유닛 테스트 완료 (#41, #81)

--- src/test/java/org/deco/gachicoding/unit/user/domain/UserRepositoryTest.java
+++ src/test/java/org/deco/gachicoding/unit/user/domain/UserRepositoryTest.java
@@ -1,19 +1,21 @@
 package org.deco.gachicoding.unit.user.domain;
 
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.deco.gachicoding.user.domain.User;
 import org.deco.gachicoding.user.domain.repository.UserRepository;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.dao.DataIntegrityViolationException;
 
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
 @DataJpaTest // JPA 에 관련된 의존성 제공, 자동으로 롤백
@@ -30,20 +32,14 @@ public class UserRepositoryTest {
     @BeforeEach
     void setUp() {
         user = MockUser.builder()
-                .userIdx(1L)
                 .userEmail("inhan1009@naver.com")
                 .build();
 
+
         userRepository.save(user);
-    }
 
-    @AfterEach
-    void tearDown() {
-
-        userRepository.deleteAll();
-//        testEntityManager.getEntityManager()
-//                .createNativeQuery("ALTER TABLE user RESTART IDENTITY")
-//                .executeUpdate();
+//        testEntityManager.flush();
+//        testEntityManager.clear();
     }
 
     @DisplayName("유저 이메일로 유저를 조회한다.")
@@ -57,7 +53,6 @@ public class UserRepositoryTest {
     @DisplayName("가입되지 않은 유저 이메일로 유저를 조회할 수 없다.")
     @Test
     public void findByUserEmail_InvalidUserEmail_Empty() {
-
         assertThat(userRepository.findByUserEmail("invalidUser")).isEmpty();
     }
 
@@ -79,23 +74,29 @@ public class UserRepositoryTest {
     @DisplayName("중복된 이메일로 유저를 저장할 수 없다.")
     @Test
     public void save_DuplicateEmail_Failed() {
+
         User duplicateEmailUser = MockUser.builder()
                 .userEmail("inhan1009@naver.com")
                 .userNick("saveTestNick")
                 .build();
 
         assertThatThrownBy(
-                () -> userRepository.save(duplicateEmailUser)
+                () -> {
+                    userRepository.save(duplicateEmailUser);
+                }
         ).isInstanceOf(DataIntegrityViolationException.class);
     }
 
     @DisplayName("유저번호로 유저를 삭제한다.")
     @Test
     public void deleteById_Success() {
-        userRepository.deleteById(1L);
+
+        Optional<User> delUser = userRepository.findByUserEmail("inhan1009@naver.com");
+
+        userRepository.deleteById(delUser.get().getUserIdx());
 
         assertThatThrownBy(
-                () -> userRepository.findById(1L)
+                () -> userRepository.findById(delUser.get().getUserIdx())
                         .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."))
         ).isInstanceOf(IllegalArgumentException.class);
     }

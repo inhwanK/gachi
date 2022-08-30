4b78f9ddc9ec9bd6f290d75694a2820c60c36a78
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-08-31 05:01:30 +0900

    Test(User) : repository 테스트 완료, service 테스트 추가 #91

--- src/test/java/org/deco/gachicoding/unit/user/application/UserServiceTest.java
+++ src/test/java/org/deco/gachicoding/unit/user/application/UserServiceTest.java
@@ -1,75 +1,96 @@
 package org.deco.gachicoding.unit.user.application;
 
-import org.deco.gachicoding.user.dto.request.UserSaveRequestDto;
+import org.deco.gachicoding.common.factory.user.MockUser;
 import org.deco.gachicoding.user.application.UserService;
+import org.deco.gachicoding.user.domain.User;
+import org.deco.gachicoding.user.domain.repository.UserRepository;
+import org.deco.gachicoding.user.dto.request.UserSaveRequestDto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
-// 서비스 테스트에서 비즈니스 로직에서 발생할 수 있는 예외 상황의 테스트를 진행한다
-@ExtendWith(MockitoExtension.class)
+
 //@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ExtendWith({MockitoExtension.class})
 public class UserServiceTest {
 
-    @Mock
+    @InjectMocks
     private UserService userService;
 
-    @Test
-    @DisplayName("UserService - 회원가입 테스트")
-    void createUser_Success() {
+    @Mock
+    PasswordEncoder passwordEncoder;
 
-//        given(userService.createUser());
+    @Mock
+    private UserRepository userRepository;
 
-        /*
-        UserSaveRequestDto userSaveRequestDto = new UserSaveRequestDto();
-        userSaveRequestDto.setEmail("ay9564@naver.com");
-        userSaveRequestDto.setName("서영준");
-        userSaveRequestDto.setPassword("ay789456");
+    @BeforeEach
+    void setUp() {
 
-        // When
-        Long idx = userService.registerUser(userSaveRequestDto);
-        UserResponseDto user = userService.getUser(idx);
-
-        // Then
-        assertEquals(user.getEmail(), userSaveRequestDto.getEmail());
-        */
     }
 
+    @DisplayName("사용자를 생성한다.")
     @Test
-    @DisplayName("UserService - 중복 아이디 회원가입 테스트")
-    void duplicationEmailJoin() {
-        // Given
-        UserSaveRequestDto userSaveRequestDto1 = new UserSaveRequestDto();
-//        userSaveRequestDto1.setEmail("ay9564@naver.com");
-//        userSaveRequestDto1.setName("서영준");
-//        userSaveRequestDto1.setPassword("ay789456");
+    void createUser_Success() {
 
-        // When
-//        Long exception_code = userService.registerUser(userSaveRequestDto1);
+        UserSaveRequestDto dto = UserSaveRequestDto.builder()
+                .userEmail("1234@1234.com")
+                .userName("InHwan")
+                .userNick("nani_inaning")
+                .userPassword("1234")
+                .build();
 
-        // exception_code(난중에 다시 정하자) : -100, message : "중복된 아이디 입니다."
-        // Then
-//        assertEquals(exception_code, -100);
+        User user = MockUser.builder()
+                .userEmail("1234@1234.com")
+                .userName("InHwan")
+                .userNick("nani_inaning")
+                .userPassword("1234")
+                .build();
+
+        given(userRepository.existsByUserEmail(any())).willReturn(false);
+        given(userRepository.save(any())).willReturn(user);
+        given(passwordEncoder.encode(any())).willReturn(any());
+
+        Long userIdx = userService.createUser(dto);
+        assertThat(userIdx).isEqualTo(user.getUserIdx());
+    }
+
+    @DisplayName("중복된 이메일을 가진 사용자는 회원가입을 할 수 없다.")
+    @Test
+    void createUser_Exception() {
+
+        UserSaveRequestDto dto = UserSaveRequestDto.builder()
+                .userEmail("1234@1234.com")
+                .userName("InHwan")
+                .userNick("nani_inaning")
+                .userPassword("1234")
+                .build();
+
+        given(userRepository.existsByUserEmail(any())).willReturn(true);
+
+        assertThatThrownBy(() -> userService.createUser(dto))
+                .isInstanceOf(DataIntegrityViolationException.class)
+                .hasMessageContaining("중복된");
+    }
+
+    @DisplayName("회원 탈퇴한다.")
+    @Test
+    void deleteUser_Success() {
+        userService.deleteUser(1L);
+        then(userRepository).should().deleteById(anyLong());
     }
 
     @Test
     @DisplayName("UserService - 이메일 형식이 아닌 아이디 회원가입 테스트")
     void notEmailFormatIdJoinUser() {
-        // Given
-        UserSaveRequestDto userSaveRequestDto1 = new UserSaveRequestDto();
-//        userSaveRequestDto1.setEmail("ay9564naver.com");
-//        userSaveRequestDto1.setName("서영준");
-//        userSaveRequestDto1.setPassword("ay789456");
-
-        // When
-//        Long exception_code = userService.registerUser(userSaveRequestDto1);
-
-        // exception_code : -200, message : "올바른 형식의 아이디가 아닙니다."
-        // Then
-//        assertEquals(exception_code, "올바른 형식의 아이디가 아닙니다.");
+        fail("미구현");
     }
 }

9daeb2fa572f1609381703e6eedf4638c3bd6534
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-08-30 22:14:44 +0900

    Fix(user) : api/user/info 응답을 위한 dto 변경

--- src/main/java/org/deco/gachicoding/user/presentation/RestUserSessionController.java
+++ src/main/java/org/deco/gachicoding/user/presentation/RestUserSessionController.java
@@ -7,28 +7,28 @@ import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.deco.gachicoding.user.application.UserService;
 import org.deco.gachicoding.user.dto.request.UserAuthenticationDto;
+import org.deco.gachicoding.user.dto.response.UserResponseDto;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Api(tags = "사용자 정보 API")
+@Api(tags = "사용자 세션 API")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api")
 public class RestUserSessionController {
-
-
 
     @ApiOperation(value = "유저정보 받기", notes = "임시로 만든 API, 인가 처리 개발되는데로 삭제 예정")
     @ApiResponses(
             @ApiResponse(code = 200, message = "세션 정보 가져오기 성공")
     )
     @GetMapping("/user/info")
-    public UserAuthenticationDto getUserInfo() {
+    public UserResponseDto getUserInfo() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         UserAuthenticationDto dto = (UserAuthenticationDto) authentication.getPrincipal();
-        return dto;
+        UserResponseDto responseDto = new UserResponseDto(dto.getUsername(), dto.getUserNick());
+        return responseDto;
     }
 }

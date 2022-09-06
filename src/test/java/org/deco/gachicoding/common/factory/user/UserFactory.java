49f6f5701743f8d1f3e635fd155b1e01382b1501
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-09-06 22:35:06 +0900

    Fix(UserFactory) : 변경된 메소드 이름 적용

--- src/test/java/org/deco/gachicoding/common/factory/user/UserFactory.java
+++ src/test/java/org/deco/gachicoding/common/factory/user/UserFactory.java
@@ -12,7 +12,7 @@ public class UserFactory {
     public static User user(Long userIdx) {
         return MockUser.builder()
                 .userIdx(userIdx)
-                .builder();
+                .build();
     }
 
     public static User user(Long userIdx, String userEmail, String userPassword) {
@@ -20,6 +20,6 @@ public class UserFactory {
                 .userIdx(userIdx)
                 .userEmail(userEmail)
                 .userPassword(userPassword)
-                .builder();
+                .build();
     }
 }

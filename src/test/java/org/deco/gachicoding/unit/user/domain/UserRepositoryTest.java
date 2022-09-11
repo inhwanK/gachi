08d22265cc08430f069ab690c8159bef5e9c2aca
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-09-12 01:12:50 +0900

    Chore(User) : 코드 정리

--- src/test/java/org/deco/gachicoding/unit/user/domain/UserRepositoryTest.java
+++ src/test/java/org/deco/gachicoding/unit/user/domain/UserRepositoryTest.java
@@ -8,22 +8,18 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.dao.DataIntegrityViolationException;
 
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.mockito.BDDMockito.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DataJpaTest // JPA 에 관련된 의존성 제공, 자동으로 롤백
 public class UserRepositoryTest {
 
     @Autowired
     UserRepository userRepository;
-
-    @Autowired
-    TestEntityManager testEntityManager;
 
     User user;
 

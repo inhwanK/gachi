c7c7a10acbb1e32501235c05f968ecd6dd71283c
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-09-13 23:11:51 +0900

    Test(EmailConfirmToken) : Service 단위 테스트 생성 #91

---
+++ src/test/java/org/deco/gachicoding/unit/emailconfirm/application/EmailConfirmTokenServiceTest.java
@@ -0,0 +1,17 @@
+package org.deco.gachicoding.unit.emailconfirm.application;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+public class EmailConfirmTokenServiceTest {
+
+    @Test
+    void createOrRenewToken() {
+        fail("미구현");
+    }
+
+    @Test
+    void checkToken() {
+        fail("미구현");
+    }
+}

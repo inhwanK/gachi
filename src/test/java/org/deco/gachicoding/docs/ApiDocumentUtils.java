bdb5a50bc7d50e651fd40a7147a86941645b599b
YeongJun Seo <ay9564@naver.com>
2022-09-19 03:56:06 +0900

    Feature(Spring REST Docs): Notice Test에 REST Docs 적용 중 (#119)

---
+++ src/test/java/org/deco/gachicoding/docs/ApiDocumentUtils.java
@@ -0,0 +1,24 @@
+package org.deco.gachicoding.docs;
+
+import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
+import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+
+public interface ApiDocumentUtils {
+
+    static OperationRequestPreprocessor getDocumentRequest() {
+//        return preprocessRequest(
+//                modifyUris()
+//                        .scheme("https")
+//                        .host("docs.api.com")
+//                        .removePort(),
+//                prettyPrint()
+//        );
+        return preprocessRequest(prettyPrint());
+    }
+
+    static OperationResponsePreprocessor getDocumentResponse() {
+        return preprocessResponse(prettyPrint());
+    }
+}

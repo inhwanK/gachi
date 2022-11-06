48d1b3e5b8454d5302814835fbfa0f58a3f15486
YeongJun Seo <ay9564@naver.com>
2022-11-06 23:06:35 +0900

    Refactor(File Upload): Article 업데이트 시 이미지를 삭제하면 디비에 저장된 메타 데이터도 함께 삭제하는 로직 추가(#134)

--- src/main/java/org/deco/gachicoding/file/application/FileService.java
+++ src/main/java/org/deco/gachicoding/file/application/FileService.java
@@ -10,10 +10,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -32,18 +35,40 @@ public class FileService {
                 fileRepository.save(file);
         }
 
-//        @Transactional
-//        public FileResponse getFiles(Long idx, String category, FileResponse dto) {
-//                List<FileResponseDto> result = new ArrayList<>();
-//                List<File> fileList = fileRepository.findAllByArticleCategoryAndArticleIdx(category, idx);
-//
-//                for (File f : fileList) {
-//                        result.add(new FileResponseDto(f));
-//                }
-//
-//                dto.setFiles(result);
-//                return dto;
-//        }
+        @Transactional
+        public void compareFilePathAndDelete(
+                Long idx,
+                String category,
+                List<String> pathInArticle
+        ) {
+                List<String> removePath = compareFilePath(
+                        pathInArticle,
+                        fileRepository.findFilePathByCategoryAndIdx(category, idx)
+                );
+
+                if (!removePath.isEmpty())
+                        removeAll(removePath);
+
+        }
+
+        private List<String> getFilePath(Long idx, String category) {
+                return fileRepository.findFilePathByCategoryAndIdx(category, idx);
+        }
+
+        private List<String> compareFilePath(List<String> pathInArticle, List<String> pathInDb) {
+                List<String> removeQueue = new LinkedList<>();
+                for (String path : pathInDb) {
+                        if (!pathInArticle.contains(path))
+                                removeQueue.add(path);
+                }
+
+                return removeQueue;
+        }
+
+        // removeAll? disableAll? -> 결정에 따라 달라 짐
+        private void removeAll(List<String> path) {
+
+        }
 
         @Transactional
         public Queue<String> imgProducer(String content) {
@@ -58,14 +83,12 @@ public class FileService {
 
                 while (matcher.find()) {
                         String img = matcher.group(1);
-                        log.info(img);
-                        imgQueue.add(img);
+                        imgQueue.add(img.replace(s3Url, ""));
                 }
 
                 return imgQueue;
         }
 
-        @Transactional
         public String imgConsumer(Long idx, String content, String category, Queue<String> queue) {
 
                 while (!queue.isEmpty()) {
@@ -84,133 +107,17 @@ public class FileService {
 
         private String uploadRealImg(Long idx, String path, String category) {
 
-                log.info("path : " + path);
+                log.info("oldPath : " + path);
 
-                String tamperingFilePath = path.replace(s3Url, "");
-
-                log.info("tamperingFileName : " + tamperingFilePath);
-
-                ObjectMetadata objectMetadata = s3Service.getObjectMetadata(tamperingFilePath);
+                ObjectMetadata objectMetadata = s3Service.getObjectMetadata(path);
 
                 File file = FileAssembler.file(idx, category, objectMetadata);
 
                 registerFile(file);
 
-                String oldPath = tamperingFilePath;
                 String newPath = file.getFilePath();
-
-                log.info("oldPath : " + oldPath);
                 log.info("newPath : " + newPath);
 
-                return s3Service.replaceS3(oldPath, newPath);
+                return s3Service.replaceS3(path, newPath);
         }
-
-//        private String uploadRealImg(Long idx, String path, String category) throws IOException {
-//                String origFileName = null;
-//                String origFileExtension = null;
-//                String tamperingFileName = null;
-//                String filePath = null;
-//                String oldPath = null;
-//                String newPath = null;
-//                Long fileSize = null;
-//
-//                // 저장 경로 바꿔야 함 (날짜도 추가)
-//                log.info("path : " + path);
-//
-//                // oldPath -> substring, indexOf, lastIndexOf -> 뒤에서 부터 인덱스 찾기
-////                try {
-////                        oldPath = URLDecoder.decode(path.substring(path.indexOf("temp")),"UTF-8");
-////                } catch (UnsupportedEncodingException e) {
-////                        log.error("Encoding Error");
-////                        throw e;
-////                }
-//
-//                oldPath = path.replace(s3Url, "");
-//                tamperingFileName = oldPath.split("temp/")[1];  //[4] -> /의 수에따라 바뀜
-//
-//                origFileName = tamperingFileName.substring(tamperingFileName.indexOf("_")).replaceFirst("_", "");
-//                origFileExtension = origFileName.substring(origFileName.lastIndexOf("."));
-//
-//                // 날짜 추가
-//                newPath = category + "/" + idx + "/" + tamperingFileName;
-//                fileSize = convertFile(path, tamperingFileName);
-//
-//                filePath = s3Service.replaceS3(oldPath, newPath);
-//                log.info("oldPath : " + oldPath);
-//                log.info("tamperingFileName : " + tamperingFileName);
-//                log.info("origFileName : " + origFileName);
-//                log.info("origFileExtension : " + origFileExtension);
-//                log.info("newPath : " + newPath);
-//                log.info("fileSize : " + fileSize);
-//                log.info("filePath : " + filePath);
-//
-//                FileSaveRequestDto dto = FileSaveRequestDto.builder()
-//                        .articleIdx(idx)
-//                        .articleCategory(category)
-//                        .originFilename(origFileName)
-//                        .fileExt(origFileExtension)
-//                        .saveFilename(tamperingFileName)
-//                        .fileSize(fileSize)
-//                        .filePath(filePath)
-//                        .build();
-//
-//                registerFile(dto);
-//
-//                return filePath;
-//        }
-
-//        private Long convertFile(String filePath, String tamperingFileName) throws IOException {
-//                try {
-//                        Long fileSize;
-//
-//                        URL url = new URL(filePath);
-//                        String canonicalPath = new java.io.File("").getCanonicalPath();
-//                        Path savePath = Paths.get(canonicalPath + "/src/main/resources/tempImg/", tamperingFileName);
-//                        InputStream is = url.openStream();
-//
-//                        copyLocalFile(is, savePath);
-//                        fileSize = getFileSize(savePath);
-//                        deleteLocalFile(savePath);
-//
-//                        log.info("Success Convert File");
-//
-//                        return fileSize;
-//                } catch (IOException e) {
-//                        log.error("Failed Convert File");
-//                        throw e;
-//                }
-//        }
-//
-//        private void copyLocalFile(InputStream is, Path savePath) throws IOException {
-//                try {
-//                        Files.copy(is, savePath);
-//                        is.close();
-//                } catch (IOException e) {
-//                        log.error("Failed Copy Local File");
-//                        throw e;
-//                }
-//        }
-//
-//        private void deleteLocalFile(Path savePath) throws IOException {
-//                try {
-//                        Files.delete(savePath);
-//                } catch (IOException e) {
-//                        log.error("Failed Delete Local File");
-//                        throw e;
-//                }
-//        }
-//
-//        private Long getFileSize(Path savePath) throws IOException {
-//                try {
-//                        log.info("start Get File Size");
-//                        Long bytes = Files.size(savePath);
-//                        Long kilobyte = bytes/1024;
-//                        Long megabyte = kilobyte/1024;
-//                        return bytes;
-//                } catch (IOException e) {
-//                        log.info("Filed Get File Size");
-//                        throw e;
-//                }
-//        }
-
 }
--- src/main/java/org/deco/gachicoding/file/domain/repository/FileRepository.java
+++ src/main/java/org/deco/gachicoding/file/domain/repository/FileRepository.java
@@ -2,9 +2,13 @@ package org.deco.gachicoding.file.domain.repository;
 
 import org.deco.gachicoding.file.domain.File;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface FileRepository extends JpaRepository<File, Long> {
-    List<File> findAllByArticleCategoryAndArticleIdx(String category, Long idx);
+
+    @Query("SELECT f.filePath FROM File f WHERE f.articleCategory = :category AND f.articleIdx = :idx")
+    List<String> findFilePathByCategoryAndIdx(@Param("category") String category, @Param("idx") Long idx);
 }

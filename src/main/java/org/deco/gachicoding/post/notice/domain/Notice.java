9be895d67c05405eb723e1b61cd82b056b46b76b
inhwanK <79566221+inhwanK@users.noreply.github.com>
2023-02-17 03:06:06 +0900

    style: 불필요한 주석 삭제

--- src/main/java/org/deco/gachicoding/post/notice/domain/Notice.java
+++ src/main/java/org/deco/gachicoding/post/notice/domain/Notice.java
@@ -38,7 +38,7 @@ public class Notice extends BaseTimeEntity {
     private NoticeContents notContents;
 
     @Column(name = "not_views", nullable = false)
-    @ColumnDefault("0") // ddl 수행시 적용되는 default 값, @Column의 columnDefinition 속성보다는 이걸 쓰는게 나을 듯
+    @ColumnDefault("0")
     private Long notViews;
 
     @Column(name = "not_pin", nullable = false)
@@ -49,12 +49,6 @@ public class Notice extends BaseTimeEntity {
     @ColumnDefault("true")
     private Boolean notEnabled;
 
-    // FetchType.EAGER 즉시 로딩
-    // 1. 대부분의 JPA 구현체는 가능하면 조인을 사용해서 SQL 한번에 함께 조회하려고 한다.
-    // 2. 이렇게 하면, 실제 조회할 때 한방 쿼리로 다 조회해온다.
-    // FetchType.LAZY 지연 로딩
-    // 1. 로딩되는 시점에 Lazy 로딩 설정이 되어있는 Team 엔티티는 프록시 객체로 가져온다.
-    // 2. 후에 실제 객체를 사용하는 시점에 초기화가 된다. DB에 쿼리가 나간다.
     @JsonManagedReference
     @JoinColumn(name = "user_idx")
     @ManyToOne(fetch = FetchType.LAZY)

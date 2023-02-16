fee3069a1394c0c1cfd4a70e5e3d8adc426ccf66
inhwanK <inhan1009@naver.com>
2023-02-16 21:11:00 +0900

    test(Question): Mock 클래스 생성 및 질문 저장 테스트 수행

---
+++ src/test/java/org/deco/gachicoding/common/factory/post/question/QuestionMock.java
@@ -0,0 +1,93 @@
+package org.deco.gachicoding.common.factory.post.question;
+
+import org.deco.gachicoding.common.factory.user.UserMock;
+import org.deco.gachicoding.post.answer.domain.Answer;
+import org.deco.gachicoding.post.question.domain.Question;
+import org.deco.gachicoding.user.domain.User;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuestionMock {
+
+    private QuestionMock() {
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+
+        private Long queIdx;
+        private User questioner = UserMock.builder().userIdx(1L).build();
+        private List<Answer> answers = new ArrayList<>();
+        private String queTitle = "QUESTION TITLE TEST";
+        private String queContents = "QUESTION CONTENTS TEST";
+        private Boolean queSolved;
+        private Boolean queEnabled;
+        private LocalDateTime queCreatedAt = LocalDateTime.now();
+        private LocalDateTime queUpdatedAt = LocalDateTime.now();
+
+        public Builder queIdx(Long queIdx) {
+            this.queIdx = queIdx;
+            return this;
+        }
+
+        public Builder questioner(User questioner) {
+            this.questioner = questioner;
+            return this;
+        }
+
+        public Builder answers(List<Answer> answers) {
+            this.answers = answers;
+            return this;
+        }
+
+        public Builder queTitle(String queTitle) {
+            this.queTitle = queTitle;
+            return this;
+        }
+
+        public Builder queContents(String queContents) {
+            this.queContents = queContents;
+            return this;
+        }
+
+        public Builder queSolved(Boolean queSolved) {
+            this.queSolved = queSolved;
+            return this;
+        }
+
+        public Builder queEnabled(Boolean queEnabled) {
+            this.queEnabled = queEnabled;
+            return this;
+        }
+
+        public Builder queCreatedAt(LocalDateTime queCreatedAt) {
+            this.queCreatedAt = queCreatedAt;
+            return this;
+        }
+
+        public Builder queUpdatedAt(LocalDateTime queUpdatedAt) {
+            this.queUpdatedAt = queUpdatedAt;
+            return this;
+        }
+
+        public Question build() {
+            return Question.builder()
+                    .queIdx(queIdx)
+                    .questioner(questioner)
+                    .queTitle(queTitle)
+                    .queContents(queContents)
+                    .queEnabled(queEnabled)
+                    .queSolved(queSolved)
+                    .queEnabled(queEnabled)
+                    .queSolved(queSolved)
+                    .queCreatedAt(queCreatedAt)
+                    .queUpdatedAt(queUpdatedAt)
+                    .build();
+        }
+    }
+}
--- src/test/java/org/deco/gachicoding/unit/post/question/domain/QuestionRepositoryTest.java
+++ src/test/java/org/deco/gachicoding/unit/post/question/domain/QuestionRepositoryTest.java
@@ -1,14 +1,80 @@
 package org.deco.gachicoding.unit.post.question.domain;
 
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Before;
+import org.deco.gachicoding.common.factory.post.question.QuestionMock;
+import org.deco.gachicoding.common.factory.user.UserMock;
+import org.deco.gachicoding.post.question.domain.Question;
 import org.deco.gachicoding.post.question.domain.repository.QuestionRepository;
+import org.deco.gachicoding.user.domain.User;
+import org.deco.gachicoding.user.domain.repository.UserRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
 @DataJpaTest
 public class QuestionRepositoryTest {
 
     @Autowired
     QuestionRepository questionRepository;
 
+    @Autowired
+    UserRepository userRepository;
 
+    User questioner = UserMock.builder()
+            .userIdx(1L)
+            .build();
+
+    @BeforeEach
+    void setUp() {
+        log.info("setUp start");
+        log.info("질문자 저장");
+        userRepository.save(questioner);
+
+        log.info("setUp end");
+
+    }
+
+    @Test
+    @DisplayName("질문을 등록한다.")
+    public void save_Question_Success() {
+        Question question = QuestionMock.builder()
+                .queIdx(1L)
+                .questioner(questioner)
+                .build();
+
+        Long saveIdx = questionRepository.save(question).getQueIdx();
+
+        assertThat(saveIdx).isEqualTo(question.getQueIdx());
+    }
+
+    @Test
+    @DisplayName("질문을 조회한다.")
+    public void find_Question_Success() {
+        Question question = QuestionMock.builder()
+                .queIdx(1L)
+                .questioner(questioner)
+                .build();
+
+        Long saveIdx = questionRepository.save(question).getQueIdx();
+        Long actualIdx = questionRepository.findById(saveIdx).get().getQueIdx();
+
+        assertThat(saveIdx)
+                .isEqualTo(actualIdx);
+    }
 }
